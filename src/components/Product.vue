7e69cf2ea4837da82f264e717badf67a3553d380
gitoutofbox <people.outofbox@gmail.com>
2021-08-10 08:03:33 +0530

    product componr

---
+++ src/components/Product.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row product-block">
+    <div class="col-sm-10">
+      <h4>{{ product.name }}</h4>
+      {{ formatPrice(product.price) }} <br />
+      {{ product.shortInfo }}
+    </div>
+    <div class="col-sm-2">
+      <img :src="product.image" alt="product.name" class="product-img" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    product: Object,
+  },
+  methods: {
+    formatPrice(value) {
+      let val = (value / 1).toFixed(2).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.product-block {
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 15px;
+}
+.product-img {
+  height: 150px;
+}
+</style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="row">
-    <div class="col-sm-12 product-block" v-for="product in products" :key="product.id">
-      <div class="row">
-        <div class="col-sm-10">
-          <h4>{{ product.name }}</h4>
-          {{ formatPrice(product.price) }} <br />
-          {{ product.shortInfo }}
-        </div>
-        <div class="col-sm-2">
-          <img :src="product.image" alt="product.name" class="product-img" />
-        </div>
-      </div>
+    <div
+      class="col-sm-12"
+      v-for="product in products"
+      :key="product.id"
+    >
+      <Product :product="product" />
     </div>
   </div>
 </template>
 
 <script>
+import Product from "@/components/Product";
 export default {
   name: "Products",
+  components: {
+    Product,
+  },
   data() {
     return {
       baseUrl: process.env.VUE_APP_BASE_URL,
@@ -35,20 +34,11 @@ export default {
       const products = await resp.json();
       this.products = products.data;
     },
-    formatPrice(value) {
-        let val = (value/1).toFixed(2).replace('.', ',')
-        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-    }
+    // formatPrice(value) {
+    //     let val = (value/1).toFixed(2).replace('.', ',')
+    //     return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+    // }
   },
 };
 </script>
 
-<style scoped>
-.product-block{
-    border-bottom: 1px solid #ddd;
-    margin-bottom: 15px;
-}
-.product-img{
-    height: 150px;
-}
-</style>
